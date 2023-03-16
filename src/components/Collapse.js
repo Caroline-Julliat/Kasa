ab464ca8debd057a3ce1d5f11070d71afd781e64
Caroline Julliat <carolinejulliat@gmail.com>
2023-03-16 22:53:28 +0100

    functional collapse component

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -3,18 +3,22 @@ import React, { useState } from "react"
 const Collapse = (props) => {
   const [isActive, setIsActive] = useState(false)
 
-  const handleCollapse = () => {
-    isActive ? setIsActive(false) : setIsActive(true)
-    console.log(isActive)
-  }
-
   return (
-    <div className="collapse-container" onClick={() => handleCollapse()}>
+    <div
+      className="collapse-container"
+      onClick={() => (isActive ? setIsActive(false) : setIsActive(true))}
+    >
       <div className="collapse-button">
         <h2>{props.title}</h2>
-        <img src="#" alt="chevron" />
+        <img
+          src={isActive ? "/images/fleche-haut.png" : "/images/fleche-bas.png"}
+          alt="chevron"
+        />
       </div>
-      <div className="collapse-content">
+      <div
+        className="collapse-content"
+        style={{ display: isActive ? "block" : "none" }}
+      >
         <p>{props.content}</p>
       </div>
     </div>
